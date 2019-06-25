a563e7ed0438eb8191ccf93c5be713f0658bd547
Dinis Jorge <djorge@alter-solutions.com>
2019-06-25 18:05:05 +0100

    :wrench: Add the new resources to the library public-api file

--- projects/filter-box-library/src/public-api.ts
+++ projects/filter-box-library/src/public-api.ts
@@ -3,3 +3,13 @@
  */
 
 export * from './lib/filter-box/filter-box.module';
+export * from './lib/filter-box/filter-box.component';
+
+export * from './lib/filter-box/entities/checkbox-filter';
+export * from './lib/filter-box/entities/default-filter';
+export * from './lib/filter-box/entities/filter';
+
+export * from './lib/filter-box/models/filter-option.model';
+export * from './lib/filter-box/models/filter-param.model';
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FlexLayoutModule} from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FilterBoxModule } from 'filter-box-library';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, FilterBoxModule, FlexLayoutModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
