50103150f7f18d6427d1f2095df4ed44591beda8
Dinis Jorge <djorge@alter-solutions.com>
2019-07-03 10:33:26 +0100

    :clown_face: Add mock data

---
+++ src/app/pizza/shared/mock.ts
@@ -0,0 +1,11 @@
+import { PizzaModel } from './pizza.model';
+
+export const PIZZASLIST: PizzaModel[] = [
+  {id: 1, name: 'Margherita', base: 'Tomato', restaurant: ''},
+  {id: 2, name: 'Marinara', base: 'Cheese', restaurant: 'Fat Chef', rating: 5},
+  {id: 3, name: 'Frutti di Mare', base: 'Seafood', restaurant: 'Little Mermaid', rating: 1},
+  {id: 4, name: 'Carbonara', base: 'Tomato', restaurant: ''},
+  {id: 5, name: 'Quattro Formaggi', base: 'Cheese', restaurant: 'The Swiss'},
+  {id: 6, name: 'Napoli', base: 'Tomato', restaurant: 'Fat Chef'},
+  {id: 7, name: 'Pugliese', base: 'Onions', restaurant: ''}
+]
