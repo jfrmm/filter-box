2c351fcfc4b85154deaa0f9b96547883bda6f596
Dinis Jorge <djorge@alter-solutions.com>
2019-10-11 16:51:26 +0100

    #16 :white_check_mark: Update date filter tests

--- projects/filter-box-library/src/lib/filter-box/filters/date-filter/date-filter.spec.ts
+++ projects/filter-box-library/src/lib/filter-box/filters/date-filter/date-filter.spec.ts
@@ -10,26 +10,4 @@ describe('DateFilter', () => {
   it('should create an instance', () => {
     expect(dateFilter).toBeDefined();
   });
-
-  it('should set the filter value', () => {
-    dateFilter.setValue('MOCK');
-
-    expect(dateFilter.elements.formControl.value).toBe('MOCK');
-  });
-
-  it('should clear the filter value', () => {
-    dateFilter.clearFilter();
-
-    expect(dateFilter.elements.formControl.value).toBe('');
-  });
-
-  it('should disable & enable the filter', () => {
-    dateFilter.disableFilter();
-
-    expect(dateFilter.elements.formControl.disabled).toBeTruthy();
-
-    dateFilter.enableFilter();
-
-    expect(dateFilter.elements.formControl.enabled).toBeTruthy();
-  });
 });
